67facd934c4467fd1597ce1ba341b848c1b10756
Muhammadjon Ibrohimov <ibrokhimovmiu@gmail.com>
2025-12-03 13:53:00 +0500

    changed

--- main/views.py
+++ main/views.py
@@ -19,6 +19,7 @@ from .serializers import (
     HomeworkSerializer
 )
 
+
 from account.models import (
     CustomUser, AdminTeacher, Student,
     CourseType, Course
@@ -28,7 +29,6 @@ from account.serializers import (
     AdminTeacherSerializer, StudentSerializer,
     CustomUserSerializer
 )
-
 
 
 # Create your views here.
@@ -42,86 +42,71 @@ class AdminEnterPermission(BasePermission):
     def has_object_permission(self, request, view, obj):
         return obj.user == request.user
 
-class AdminHomeView(viewsets.ViewSet):
 
+class AdminHomeView(viewsets.ViewSet):
     permission_classes = [AdminEnterPermission]
 
-
     def list(self, request):
-        info = get_list_or_404(AdminTeacher)
-        info = AdminTeacherSerializer(info, many=True).data
-        print(info)
-        for data in info:
-            data['user'] = CustomUserSerializer(get_object_or_404(CustomUser, pk=data['user'])).data
-
-        return Response(info)
-
+        queryset = AdminTeacher.objects.select_related("user").all()
+        data = AdminTeacherSerializer(queryset, many=True).data
+        return Response(data, status=status.HTTP_200_OK)
 
     def retrieve(self, request, pk):
-        info = get_object_or_404(AdminTeacher, pk=pk)
-        info = AdminTeacherSerializer(info).data
-        info['user'] = CustomUserSerializer(get_object_or_404(CustomUser, pk=info['user'])).data
-        return Response(info, status=status.HTTP_200_OK)
-
+        obj = get_object_or_404(AdminTeacher.objects.select_related("user"), pk=pk)
+        data = AdminTeacherSerializer(obj).data
+        return Response(data, status=status.HTTP_200_OK)
 
     def create(self, request):
-        data = request.data
-        user = data['user']
-        user_serializer = CustomUserSerializer(data=user)
+        user_data = request.data.get("user")
+        user_serializer = CustomUserSerializer(data=user_data)
 
         if not user_serializer.is_valid():
             return Response(user_serializer.errors, status=status.HTTP_400_BAD_REQUEST)
-        
-        user_serializer.save()
-        print(user_serializer.data)
-        data['user'] = user_serializer.data['id']
-        admin_teacher_serializer = AdminTeacherSerializer(data=data)
 
-        if not admin_teacher_serializer.is_valid():
-            return Response(admin_teacher_serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+        user = user_serializer.save()
 
-        admin_teacher_serializer.save()
-        data = admin_teacher_serializer.data
-        data['user'] = CustomUserSerializer(CustomUser.objects.get(pk=data['user'])).data
-        
-        return Response({"message":"success", "data":data}, status=status.HTTP_200_OK)
-    
+        admin_data = request.data.copy()
+        admin_data["user"] = user.id
+
+        admin_serializer = AdminTeacherSerializer(data=admin_data)
+        if not admin_serializer.is_valid():
+            return Response(admin_serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+        admin = admin_serializer.save()
+        return Response(
+            {"message": "success", "data": AdminTeacherSerializer(admin).data},
+            status=status.HTTP_201_CREATED
+        )
 
     def update(self, request, pk):
+        admin_instance = get_object_or_404(AdminTeacher, pk=pk)
 
-        data = request.data
+        user = admin_instance.user
+        user_data = request.data.get("user")
 
-        admin_teacher_instance = get_object_or_404(AdminTeacher, pk=pk)
+        partial = request.method == 'PATCH'
 
-        user = CustomUser.objects.get(id=admin_teacher_instance.user.id)
-        user_data = data['user']
-
-        if request.method == 'PUT':
-            user_serializer = CustomUserSerializer(instance= user,data=user_data, partial=False)
-
-        else:
-            user_serializer = CustomUserSerializer(instance= user,data=user_data, partial=True)
-
+        user_serializer = CustomUserSerializer(user, data=user_data, partial=partial)
         if not user_serializer.is_valid():
-            return Response(user_serializer.errors, status=status.HTTP_406_NOT_ACCEPTABLE)
+            return Response(user_serializer.errors, status=status.HTTP_400_BAD_REQUEST)
         user_serializer.save()
 
-        data['user'] = user_serializer.data['id']
-        admin_serializer = AdminTeacherSerializer(instance=admin_teacher_instance ,data=data, partial=False)
+        admin_data = request.data.copy()
+        admin_data["user"] = user.id
 
+        admin_serializer = AdminTeacherSerializer(admin_instance, data=admin_data, partial=partial)
         if not admin_serializer.is_valid():
-            return Response(admin_serializer.errors, status=status.HTTP_406_NOT_ACCEPTABLE)
+            return Response(admin_serializer.errors, status=status.HTTP_400_BAD_REQUEST)
         
         admin_serializer.save()
 
         return Response(admin_serializer.data, status=status.HTTP_202_ACCEPTED)
 
-
-
-
-
-
-
-# class
+    def destroy(self, request, pk):
+        admin_teacher = get_object_or_404(AdminTeacher, pk=pk)
+        
+        admin_teacher.delete()
+        return Response({"message": "deleted"}, status=status.HTTP_204_NO_CONTENT)
+    
 
 
